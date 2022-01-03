46b50c740e5aed37e899a3245f75006940f38e9b
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-01-02 22:41:48 -0300

    fixing keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,7 +3,7 @@ local map = vim.api.nvim_set_keymap
 vim.g.mapleader = ' '
 
 -- Geral
-map("n", "<C-A>", ":NvimTreeToggle<CR>", { silent = true }) 
+map("n", "<C-A>", ":NvimTreeToggle<CR>", { silent = true })
 map("n", "<leader>v ", ":VisitTestFile<CR>", { silent = true })
 
 -- Terminal
@@ -20,7 +20,7 @@ map("v", "<leader>ca", ":<C-U>Lspsaga range_code_action<CR>", { silent = true })
 map("n", "<leader>ca", ":Lspsaga code_action<CR>", { silent = true })
 
 -- Telescope
-map("n", "<M-C-P>  ", ":Telescope live_grep<CR>", { silent = true })
+map("n", "<C-M-P>", ":Telescope live_grep<CR>", { silent = true })
 map("n", "<C-P>", ":Telescope find_files<CR>", { silent = true })
 
 -- Tab navigation
@@ -34,7 +34,6 @@ map("n", "<M-4>", "4gt", { silent = true })
 map("n", "<M-3>", "3gt", { silent = true })
 map("n", "<M-2>", "2gt", { silent = true })
 map("n", "<M-1>", "1gt", { silent = true })
-
 
 -- LSP
 map("n", "gd", ":lua vim.lsp.buf.definition()<CR>", { silent = true })
