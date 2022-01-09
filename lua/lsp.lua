8bcfebe28c95a662634c0a167ec2e097b3fcdcdf
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-01-09 09:54:34 -0300

    enable autocomplete for text in others buffers

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -22,7 +22,16 @@ cmp.setup({
 		{ name = 'nvim_lsp' },
 		{ name = 'ultisnips' },
 	}, {
-		{ name = 'buffer' },
+		{
+			name = 'buffer',
+			get_bufnrs = function()
+				local bufs = {}
+				for _, win in ipairs(vim.api.nvim_list_wins()) do
+					bufs[vim.api.nvim_win_get_buf(win)] = true
+				end
+				return vim.tbl_keys(bufs)
+			end
+		},
 	}),
 	cmp.setup.cmdline('/', {
 		sources = {
