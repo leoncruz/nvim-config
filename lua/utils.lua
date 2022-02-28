786ba3bbed41a81041e8f23d2bc8bde874131f78
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-02-28 13:32:56 -0300

    show hidden files on telescope

--- lua/utils.lua
+++ lua/utils.lua
@@ -12,7 +12,13 @@ telescope.setup {
 	defaults =  {
 		file_ignore_patterns = {
 			'node_modules', '.elixir_ls', '_build', 'deps', '__pycache__',
-			'.idea', '.dart_tool', 'build'
+			'.idea', '.dart_tool', 'build', '.git'
+		}
+	},
+
+	pickers = {
+		find_files = {
+			hidden = true
 		}
 	}
 }
