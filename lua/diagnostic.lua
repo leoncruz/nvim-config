3334f1f8687a1ddf671f2614ac7a8010126f478f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-04-16 23:32:40 -0300

    remove old files

--- lua/diagnostic.lua
+++
@@ -1,8 +0,0 @@
--- disable virtual_text for lsp diagnostic
-vim.diagnostic.config({
-	virtual_text = false,
-})
-
--- use Lspsaga diagnostic
-vim.o.updatetime = 250
-vim.cmd [[ autocmd CursorHold * :Lspsaga show_line_diagnostics ]]
