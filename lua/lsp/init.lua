8a2bc16e62a5bf21d1a26b83ba8c75719e6be3ff
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-04-16 23:37:54 -0300

    fixing import names

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,7 +1,7 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local lsp_installer = require('lsp2.installer')
-local cmp = require('lsp2.nvim_cmp')
-local servers = require('lsp2.servers')
+local lsp_installer = require('lsp.installer')
+local cmp = require('lsp.nvim_cmp')
+local servers = require('lsp.servers')
 
 local servers_options = {
 	capabilities = capabilities,
