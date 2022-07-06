b317ddceab9e36ed8d00f4985955a2718dbd263f
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-07-06 10:58:17 -0300

    remove autoformat event

--- lua/events.lua
+++ lua/events.lua
@@ -1,5 +1,2 @@
--- format on save
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
-
 -- use Lspsaga diagnostic
 vim.cmd [[ autocmd CursorHold * :Lspsaga show_line_diagnostics ]]
