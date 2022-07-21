56af1f03035014f8307cc6975fe2e4f3ba738031
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-07-21 18:14:51 -0300

    use new nvim api to create autocommands

--- lua/events.lua
+++ lua/events.lua
@@ -1,2 +1,16 @@
--- use Lspsaga diagnostic
-vim.cmd [[ autocmd CursorHold * :Lspsaga show_line_diagnostics ]]
+local create_autocmd  = vim.api.nvim_create_autocmd
+
+if vim.fn.has "nvim-0.7" then
+	create_autocmd(
+		{ "VimEnter" },
+		{ pattern = "*", command = "Lspsaga show_line_diagnostics" }
+	)
+
+	create_autocmd(
+		{ "VimEnter" },
+		{ pattern = "*", command = "lua vim.diagnostic.config({ virtual_text = false })" }
+	)
+else
+	vim.cmd [[ autocmd VimEnter * :lua vim.diagnostic.config({ virtual_text = false }) ]]
+	vim.cmd [[ autocmd CursorHold * :Lspsaga show_line_diagnostics ]]
+end
