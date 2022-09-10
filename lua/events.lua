72c7606d21d4acb125e5868e0eddf1b48aa96970
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-09-10 13:42:45 -0300

    event to fix eslint corrections

--- lua/events.lua
+++ lua/events.lua
@@ -10,7 +10,10 @@ if vim.fn.has "nvim-0.7" then
 		{ "VimEnter" },
 		{ pattern = "*", command = "lua vim.diagnostic.config({ virtual_text = false })" }
 	)
+
+	create_autocmd({ 'BufWritePre' }, { pattern = "*.tsx,*.ts,*.jsx,*.js", command = 'EslintFixAll' })
 else
 	vim.cmd [[ autocmd VimEnter * :lua vim.diagnostic.config({ virtual_text = false }) ]]
 	vim.cmd [[ autocmd CursorHold * :Lspsaga show_line_diagnostics ]]
+	vim.cmd [[ autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll ]]
 end
