04a6e0a353d187b525b447c649e3826e3a7ee415
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-10-13 00:38:43 -0300

    feat: add workspaces to alpha

--- lua/plugins/settings/alpha.lua
+++ lua/plugins/settings/alpha.lua
@@ -7,11 +7,8 @@ local alpha_theme = require('alpha.themes.dashboard')
 alpha_theme.section.buttons.val = {
 	alpha_theme.button("e", "  New file", "<cmd>ene <CR>"),
 	alpha_theme.button("f f", "  Find file", '<cmd>Telescope find_files<CR>'),
-	alpha_theme.button("f h", "  Recently opened files"),
-	alpha_theme.button("f r", "  Frecency/MRU"),
+	alpha_theme.button("f h", "  Projects", '<cmd>Telescope workspaces<CR>'),
 	alpha_theme.button("f g", "  Find word", '<cmd>Telescope live_grep<CR>'),
-	alpha_theme.button("f m", "  Jump to bookmarks"),
-	alpha_theme.button("s l", "  Open last session"),
 	alpha_theme.button('q', '  Quit NVIM', ':qa<CR>'),
 }
 
