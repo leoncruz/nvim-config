333166072cc7ee55618e46f1ed35286013d20876
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-01-21 14:06:37 -0300

    update events

--- lua/user/events.lua
+++ lua/user/events.lua
@@ -1,14 +1,5 @@
-if vim.fn.has "nvim-0.7" then
-	local create_autocmd  = vim.api.nvim_create_autocmd
+local create_autocmd  = vim.api.nvim_create_autocmd
 
+if vim.fn.exists(':EslintFixAll') > 0 then
 	create_autocmd({ 'BufWritePre' }, { pattern = "*.tsx,*.ts,*.jsx,*.js", command = 'EslintFixAll' })
-
-	local path = vim.fn.stdpath('config') .. '/lua/plugins/init.lua'
-
-	create_autocmd(
-		{ 'BufWritePost' },
-		{ pattern = path, command = 'source <afile> | PackerCompile' }
-	)
-else
-	vim.cmd [[ autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll ]]
 end
