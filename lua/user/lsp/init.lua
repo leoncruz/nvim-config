e9469800243bc270c4668f70e45570bd67daffae
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-01-21 14:05:26 -0300

    update cmp function

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,4 +1,4 @@
-local cmp_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lsp_config = require('lspconfig')
 
 local cmp = require('user.lsp.nvim_cmp')
