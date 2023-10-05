6ecd8a96a0292c843b94ffd3cdd7e537b07994ef
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-10-05 12:08:25 -0300

    disable highlight on elixir html files

--- lua/user/plugins/settings/nvim_treesitter.lua
+++ lua/user/plugins/settings/nvim_treesitter.lua
@@ -20,6 +20,7 @@ treesitter.setup {
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false
+    additional_vim_regex_highlighting = false,
+    disable = { 'heex', 'eex' }
   }
 }
